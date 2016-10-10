9d7264bd86fe39b1d5c3623509880ea62813f5f6
ldc14 <ldc14@software.nju.edu.cn>
2016-10-10 23:21:33 +0800

    加servlet

--- FinancialMaster/src/main/java/servlet/strategy/PerfectStrategyServlet.java
+++ FinancialMaster/src/main/java/servlet/strategy/PerfectStrategyServlet.java
@@ -3,6 +3,7 @@ package servlet.strategy;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 
+import DAO.pojo.UserStrategy;
+import DAO.pojo.UserStrategyId;
+import PO.StrategyPO;
 import PO.profitPO;
+import VO.UserVO;
+import web.bl.StrategyHandle.GetStrategyImpl;
 import web.bl.StrategyHandle.PerfectStrategyHandle;
+import web.bl.StrategyHandle.StrategyHandle;
+import web.blservice.StrategyHandleService.GetStrategyInfo;
+import web.blservice.StrategyHandleService.StrategyHandleService;
 import web.blservice.StrategyHandleService.perfectStrategyService;
 
 /**
@@ -22,31 +31,36 @@ import web.blservice.StrategyHandleService.perfectStrategyService;
 @WebServlet("/PerfectStrategyServlet")
 public class PerfectStrategyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PerfectStrategyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		perfectStrategyService psService=new PerfectStrategyHandle();
-		String stockId=request.getParameter("stockId");
-		String startTime=request.getParameter("startTime");
-		String endTime=request.getParameter("endTime");
-		String cost=request.getParameter("cost");
-		ArrayList<profitPO> result=psService.getProfit(stockId, startTime, endTime, Double.parseDouble(cost));
-	    int size=result.size();
-	    profitPO po=result.get(size-1);
-	    double profit=po.getProfit();
-	    
-	    String data = "[{'profit':" +  profit + "}]";
-		
+	public PerfectStrategyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		perfectStrategyService psService = new PerfectStrategyHandle();
+		String stockId[] = request.getParameterValues("stockId[]");
+		String startTime[] = request.getParameterValues("startTime[]");
+		String endTime[] = request.getParameterValues("endTime[]");
+		String cost[] = request.getParameterValues("cost[]");
+		double profit = 0;
+		for (int i = 0; i < stockId.length; i++) {
+			ArrayList<profitPO> result = psService.getProfit(stockId[i], startTime[i], endTime[i],
+					Double.parseDouble(cost[i]));
+			int size = result.size();
+			profitPO po = result.get(size - 1);
+			profit += po.getProfit();
+		}
+		String data = "[{'profit':" + profit + "}]";
+
 		JSONArray json = new JSONArray(data);
 		PrintWriter out = response.getWriter();
 		out.println(json);
@@ -55,11 +69,72 @@ public class PerfectStrategyServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String strategyName = request.getParameter("StrategyName");
+		UserVO userVO = (UserVO) request.getSession().getAttribute("User");
+		GetStrategyInfo getStrategyInfo = new GetStrategyImpl();
+		List<UserStrategy> strategies = getStrategyInfo.getStrategy(userVO.getUsername(), strategyName);
+		UserStrategy userStrategy = strategies.get(0);
+		UserStrategyId userStrategyId = userStrategy.getId();
+		String name = userStrategyId.getStrategyName();// 策略名
+		String totalcost = userStrategy.getCost() + "";// 总成本
+		String perST = "";// 股票名称、投资成本、开始日期、结束日期、买卖频率
+		String BuyList = "";// 价格区间、成交量区间、换手率区间、pe区间、pb区间
+		String SoldList = "";
+
+		for (UserStrategy userStrategy2 : strategies) {
+			userStrategyId = userStrategy2.getId();
+			perST += userStrategyId.getStockId() + "," + userStrategy2.getWeight() + "," + userStrategy2.getStarttime()
+					+ "," + userStrategy2.getEndtime() + "," + userStrategy2.getFrequency() + ";";
+			BuyList += userStrategy2.getBuystrategy() + ";";
+			SoldList += userStrategy2.getSellstrategy() + ";";
+		}
+		String[] perSTs = perST.split(";");
+		String[] BuyLists = BuyList.split(";");
+		String[] SoldLists = SoldList.split(";");
+
+		int size = perSTs.length;
+
+		ArrayList<StrategyPO> arrayList_buy = new ArrayList<StrategyPO>();
+		ArrayList<StrategyPO> arrayList_sold = new ArrayList<StrategyPO>();
+
+		for (int i = 0; i < size; i++) {
+			String[] per = perSTs[i].split(",");// 股票名称、投资成本、开始日期、结束日期、买卖频率
+			String[] buy = BuyLists[i].split(",");// 价格区间、成交量区间、换手率区间、pe区间、pb区间
+			String[] sold = SoldLists[i].split(",");
+			StrategyPO buyPO = new StrategyPO(per[0], per[2], per[3], Double.parseDouble(buy[0]),
+					Double.parseDouble(buy[1]), Double.parseDouble(buy[2]), Double.parseDouble(buy[3]),
+					Double.parseDouble(buy[4]), Double.parseDouble(buy[5]), Double.parseDouble(buy[6]),
+					Double.parseDouble(buy[7]), Double.parseDouble(buy[8]), Double.parseDouble(buy[9]),
+					Integer.parseInt(per[4]), Double.parseDouble(per[1]));
+			arrayList_buy.add(buyPO);
+			StrategyPO soldPO = new StrategyPO(per[0], per[2], per[3], Double.parseDouble(sold[0]),
+					Double.parseDouble(sold[1]), Double.parseDouble(sold[2]), Double.parseDouble(sold[3]),
+					Double.parseDouble(sold[4]), Double.parseDouble(sold[5]), Double.parseDouble(sold[6]),
+					Double.parseDouble(sold[7]), Double.parseDouble(sold[8]), Double.parseDouble(sold[9]),
+					Integer.parseInt(per[4]), Double.parseDouble(per[1]));
+			arrayList_sold.add(soldPO);
+		}
+
+		StrategyHandleService strategyHandleService = new StrategyHandle();
+
+		ArrayList<profitPO> profitPOs = strategyHandleService.handle(arrayList_buy, arrayList_sold);
+		int length = profitPOs.size();
+		profitPO po = profitPOs.get(length - 1);
+
+		String data = "[{'profit':" + po.getProfit() + "}]";
+
+		JSONArray json = new JSONArray(data);
+		PrintWriter out = response.getWriter();
+		out.println(json);
+		out.flush();
+		out.close();
+
 	}
 
 }
